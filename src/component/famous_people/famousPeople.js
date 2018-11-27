adcf61da09a184105f0aa541f56c2fe3fd772cc3
phiathao <phiat11@gmail.com>
2018-11-27 17:03:37 -0600

    handle change for state

--- src/component/famous_people/famousPeople.js
+++ src/component/famous_people/famousPeople.js
@@ -5,20 +5,32 @@ class FamousPeople extends Component {
         person: {
             name: 'Flavius Valerius Aurelius Constantinus Augustus',
             role: 'Roman Emperor',
-            for: ' the first Roman emperor to claim conversion to Christianity'
+            for: ' Romans conversion to Christianity'
         }
     }
-    render(){
+    handleChangeFor = (propertyName) => {
+        return (event) => {
+            this.setState(
+                {
+                    person: {
+                        ...this.state.person,
+                        [propertyName]: event.target.value
+                    }
+                }
+            )
+        }
+    }
+    render() {
         return (
             <div>
                 <div>
-                  <input type='text' className='form-control' aria-label='Small' aria-describedby='inputGroup-sizing-sm' placeholder='Name' />
-                  <input type='text' className='form-control' aria-label='Small' aria-describedby='inputGroup-sizing-sm' placeholder='Role' />
-                  <input type='text' className='form-control' aria-label='Small' aria-describedby='inputGroup-sizing-sm' placeholder='Famous For' />
-                  <button type="button" class="btn btn-outline-primary">Submit</button>
+                    <input type='text' id='nameIn' className='form-control' aria-label='Small' aria-describedby='inputGroup-sizing-sm' placeholder='Name' onChange={this.handleChangeFor('name')}/>
+                    <input type='text' id='roleIn' className='form-control' aria-label='Small' aria-describedby='inputGroup-sizing-sm' placeholder='Role' onChange={this.handleChangeFor('role')}/>
+                    <input type='text' id='forIn' className='form-control' aria-label='Small' aria-describedby='inputGroup-sizing-sm' placeholder='Famous For' onChange={this.handleChangeFor('for')}/>
+                    <button type='button' class='btn btn-outline-primary' >Submit</button>
                 </div>
                 <div id='insertOut'>
-                    <p>{this.state.person.name} is famous for{this.state.person.for} as the {this.state.person.role}</p>
+                    <p>{this.state.person.name} is famous for {this.state.person.for} as the {this.state.person.role}</p>
                 </div>
             </div>
         );
